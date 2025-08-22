3572bff7e58180424475a228b0133ba1ce8d6a89
htsurev <160770031+htsurev@users.noreply.github.com>
2025-08-22 09:04:57 +0100

    upload Revue Crafters tests

---
+++ DTOs/ApiResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevueCrafters.Tests.DTOs
+{
+    public class ApiResponseDTO
+    {
+        public string? Msg { get; set; }
+        public string? RevueId { get; set; }
+    }
+}
